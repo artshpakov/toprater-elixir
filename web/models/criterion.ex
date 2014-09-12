5fe10ce195f2872d84753cdf5b993a8fccabebe9
Artem Shpakov <artshpakov@yandex.ru>
2014-09-13 01:47:39 +0400

    Making records out of criteria

--- web/models/criterion.ex
+++ web/models/criterion.ex
@@ -1,9 +1,19 @@
 defmodule Criterion do
   use RemoteModel
+  defstruct name: nil, label: nil
   
   def tree(lang \\ "ru") do
     {:ok, criteria} = fetch("criteria", [lang: lang, subcriteria: true])
     criteria["criteria"]
   end
+
+  def list(lang \\ "ru") do
+    Enum.flat_map tree(lang), fn(root) ->
+      Enum.map root["subcriteria"], fn(sub) ->
+        %Criterion{name: sub["name"], label: sub["label"]}
+      end
+    end
+  end
+  
   
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,4 +1,9 @@
 defmodule Toprater.PageView do
   use Toprater.Views
 
+  def criteria_list(lang \\ "ru") do
+    Criterion.list lang
+  end
+  
+
 end
