4070657c767c55f83aac17cfee3ca5270507aed8
Artem Shpakov <artshpakov@yandex.ru>
2014-09-15 21:58:07 +0400

    Passing data into javascript

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,7 +3,7 @@ defmodule Toprater.PageController do
   use Phoenix.Controller
 
   def index(conn, _params) do
-    render conn, "index", criteria: Criterion.tree(_params["lang"])
+    render conn, "index", tree: Criterion.tree(_params["lang"])
   end
 
 end
